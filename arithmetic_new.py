dd91ca38f6ab2bc35d2bf16f741f29d20b053c00
Hackbright Student <no-reply@hackbrightacademy.com>
2019-07-10 17:15:59 -0700

    Created add and subtract functions with list input option

---
+++ arithmetic_new.py
@@ -0,0 +1,67 @@
+"""Math functions for calculator."""
+
+
+def add(nums):
+    """Return the sum of the two inputs."""
+    sum = 0
+    for num in nums:
+        sum += num
+    return sum
+
+
+def subtract(nums):
+    """Return the second number subtracted from the first."""
+    difference = nums[0]
+    for index, num in enumerate(nums):
+        if index == 0:
+            difference = difference
+        else:
+            difference -= num
+    return difference
+
+    # difference = nums[0]
+    # for num in nums:
+    #     difference -= num
+    # difference += nums[0]
+    # return difference
+
+def multiply(num1, num2):
+    """Multiply the two inputs together."""
+    product = num1 * num2
+    return product
+
+def divide(num1, num2):
+    """Divide the first input by the second and return the result."""
+    quotient = num1/num2
+    return quotient
+
+def square(num1):
+    """Return the square of the input."""
+    squared = num1 **2
+    return squared
+
+def cube(num1):
+    """Return the cube of the input."""
+    cubed = num1 ** 3
+    return cubed
+
+
+def power(num1, num2):
+    """Raise num1 to the power of num2 and return the value."""
+    exponentiated = num1 ** num2
+    return exponentiated
+
+def mod(num1, num2):
+    """Return the remainder of num1 / num2."""
+    remainder = num1 % num2
+    return remainder
+
+def add_mult(num1, num2, num3):
+    """Return the product of num3 and the sum of num1 and num2"""
+    product = (num1 + num2) * num3
+    return product
+
+def add_cubes(num1, num2):
+    """Return the sum of the cubes of num1 and num2"""
+    sum = cube(num1) + cube(num2)
+    return sum
